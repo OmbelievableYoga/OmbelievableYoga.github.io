6de150838210241fc7a598e8288035a7b0e589ff
wsherry <sherryguiwang@gmail.com>
2018-07-16 01:21:25 -0400

    Page routing & Ash's HTML with modification added

--- OmbelievableYoga/app/controllers/poses_controller.rb
+++ OmbelievableYoga/app/controllers/poses_controller.rb
@@ -1,6 +1,7 @@
 class PosesController < ApplicationController
   def index
     @poses = Pose.all
+  end
   def show
     @pose = Pose.find(params[:id])
   end
--- OmbelievableYoga/config/routes.rb
+++ OmbelievableYoga/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'about/index'
+  get 'faq/index'
   get 'welcome/index'
+  get 'donations/index'
+  get 'workouts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :poses, :only => [:index]
+  resources :poses, :only => [:index, :show]
   root 'welcome#index'
 end
